021eb74e64273fc12efcdc1904bb165f4604b88a
Thiago Fontoura <thiagoatf@hotmail.com>
2022-09-08 06:00:52 -0300

    Update animations and more
    +Blend tree animations
    +TankControl Modified
    +PSX Assets

--- Assets/Thiago/Scripts/TankControl.cs
+++ Assets/Thiago/Scripts/TankControl.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 public class TankControl : MonoBehaviour
 {
     public GameObject player;
-    bool isMoving;
-    bool pressedD;
-    bool isTurning = false;
+    public bool isMoving;
+    public bool isBacking;
     bool isRunning = false;
     float horizontalMove;
-    public float verticalMove = 3.8f;
+    float verticalMove;
+    bool canStopAnim;
+    public float verticalSpeed = 3.8f;
     public float horizontalSpeed = 150f;
 
+    Animator anim;
+    float velocityX = 0.0f;
+    float velocity = 0.0f;
+    public float acceleration = 0.1f;
+    public float deceleration = 0.1f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.D) && !isTurning)
-        {
-            pressedD = true;            
-        }      
-        if (Input.GetKey(KeyCode.A) && !isTurning)
-        {
-            pressedD = false;
-        }
+        MovementAnimation();
+        anim.SetFloat("Velocity", velocity);
+        anim.SetFloat("VelocityX", velocityX);
 
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetButton("Run"))
         {
+
+            if (Input.GetButton("Run") && isBacking)
+            {
+                StartCoroutine(stopMove());
+                 player.GetComponent<Animator>().Play("QuickTurn");
+            }
             isRunning = true;
         }
         else
@@ -36,67 +48,102 @@ public class TankControl : MonoBehaviour
         }
 
 
-        if (Input.GetButton("Horizontal"))
-        {
-            isTurning = true;
-            player.transform.Rotate(0, horizontalMove, 0);
+        if (Input.GetButton("Horizontal") && !canStopAnim)
+        {         
             horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
-
-            if (!isMoving)
-            {
-                if (pressedD)
-                {
-                    player.GetComponent<Animator>().Play("Right");
-                }
-                else
-                {
-                    player.GetComponent<Animator>().Play("Left");
-                }               
-            }            
+            player.transform.Rotate(0, horizontalMove, 0);
         }
-        else
-        {
-            isTurning = false;
-        }
+     
 
 
-        if (Input.GetButton("Vertical"))
+        if (Input.GetButton("Vertical") && !canStopAnim)
         {
-           
+
             isMoving = true;
 
             if (Input.GetButton("SKey"))
             {
-                player.GetComponent<Animator>().Play("WalkBack");
+                verticalSpeed = 1.5f;
+                isBacking = true;                
             }
             else
             {
-                if(!isRunning)
+                isBacking = false;
+                if (!isRunning)
                 {
-                    verticalMove = 3f;
-                    player.GetComponent<Animator>().Play("Walk");
+                    verticalSpeed = 3f;                   
                 }
                 else
                 {
-                    verticalMove = 8f;
-                    player.GetComponent<Animator>().Play("Run");
+                    verticalSpeed = 8f;                   
                 }
-            }         
-
-            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * verticalMove;  
-            player.transform.Translate(0,0, verticalMove);
+            }
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
+            player.transform.Translate(0, 0, verticalMove);
         }
         else
         {
+            isBacking = false;
             isMoving = false;
         }
 
 
-        if(!isMoving && !isTurning)
+        IEnumerator stopMove()
         {
-            
-            player.GetComponent<Animator>().Play("Idle");
+            canStopAnim = true;
+            yield return new WaitForSeconds(1.18f);
+            canStopAnim = false;
         }
-        
+
+        void MovementAnimation()
+        {
+            bool forwardPressed = Input.GetKey("w");
+            bool rightPressed = Input.GetKey("d");
+            bool leftpressed = Input.GetKey("a");
+            bool runPressed = Input.GetKey("left shift");
+            if (forwardPressed && velocity < 2.0f)
+            {
+                velocity += Time.deltaTime * acceleration;
+            }
+            if (forwardPressed && runPressed && velocity < 3.0f)
+            {
+                velocity += Time.deltaTime * acceleration;
+            }
+            if (!forwardPressed && velocity > 1.0f)
+            {
+                velocity -= Time.deltaTime * deceleration;
+            }
+            if (!forwardPressed && !isBacking && velocity < 1.0f)
+            {
+                velocity += Time.deltaTime * acceleration;
+            }
+            if (isBacking && velocity > 0.0f)
+            {
+                velocity -= Time.deltaTime * deceleration;
+            }
+
+
+
+
+
+
+            if (rightPressed && !leftpressed && !forwardPressed && !isBacking && velocityX < 1.0f)
+            {
+                velocityX += Time.deltaTime * acceleration;
+            }
+            if (!rightPressed && leftpressed && !forwardPressed && velocityX > -1.0f)
+            {
+                velocityX -= Time.deltaTime * deceleration;
+            }
+            if (!rightPressed && !leftpressed && velocityX < 0.0f)
+            {
+                velocityX += Time.deltaTime * acceleration;
+            }
+            if (!rightPressed && !leftpressed && velocityX > 0.0f)
+            {
+                velocityX -= Time.deltaTime * deceleration;
+            }
+        }
+
     }
 }
