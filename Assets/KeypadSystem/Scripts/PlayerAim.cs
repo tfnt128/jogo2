9686745c37c306e7bb2a8de7f2f7bf74af926495
Thiago Fontoura <thiagoatf@hotmail.com>
2022-11-13 18:38:05 -0300

    Door and key fixed

---
+++ Assets/KeypadSystem/Scripts/PlayerAim.cs
@@ -0,0 +1,48 @@
+﻿using Cinemachine;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAim : MonoBehaviour
+{
+    public Transform headPos;
+    public Camera camera;
+    public Camera cameraZoom;
+
+    private void Update()
+    {
+
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 20, Color.green);
+
+
+            
+                Vector2 localPoint = hit.textureCoord;
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                Ray _ray = cameraZoom.ViewportPointToRay(localPoint);
+                RaycastHit _hit;
+                Debug.DrawRay(_ray.origin, _ray.direction * 20, Color.green);
+                if (Physics.Raycast(_ray, out _hit))
+                {
+
+                    if (_hit.transform.GetComponent<KeypadKey>() != null)
+                    {
+                        Debug.Log("dafeewf");
+                        _hit.transform.GetComponent<KeypadKey>().SendKey();
+                    }
+                }
+            }
+
+
+            }
+        
+    }
+}
